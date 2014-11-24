0c59990dd4a5bfc79b2e78f0ed26ea32be37f1ed
Michael Perry <lmperry@stanford.edu>
2014-11-24 11:23:12 -0800

    ENH: Move still working message to the main page and remove dependence on the gif file's existence.

--- includes/getGif.php
+++ includes/getGif.php
@@ -13,17 +13,5 @@
           ?> <!--<h2 id="<? $session ?>"><a href="#">Fiber Rendering</a></h2> --> <?
           echo "<img src=\" " . $thegif . " \" align=\"center\"</img><br>" ;
         }
-
-        // We will consider the gif a proxy for a completed session
-        // If there is no gif && there is not a mrq directory, then we assume it's still working.
-        // TODO: A beter proxy for this might be to look for the poc file in the root directory.
-        if ( (!is_dir($mrqdir)) && (!file_exists($thegif)) ) {
-          echo "<h2>Still working...</h2>"; 
-            if (file_exists($logfile)) {
-                echo "You can track progress by viewing the <a href=\"log.php\">log file.</a><br>&nbsp;";
-            }
-        }
-
-        
     }
 ?>
