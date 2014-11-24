7227f25c0cb426da579e3c7760bfd8c38e744be9
Michael Perry <lmperry@stanford.edu>
2014-11-24 12:39:50 -0800

    ENH: Add graph details to image title.

--- includes/getSummaryFigures.php
+++ includes/getSummaryFigures.php
@@ -1,13 +1,11 @@
 
 <?php
 
-    function getSummaryFigures($session) 
+    function getSummaryFigures($session)
     {
-      
+
       include '/var/www/includes/variables.php';
 
-      
-      
       $summary_dir = $figures_dir . "/" . $session . "/" ;
       // TODO: Check if this dir exists.
 
@@ -18,54 +16,18 @@
       {
         echo "<p><h3><a href=\"#\" id=\"figures\">Summary Figures</a></h3>";
       }
-      
-      foreach($images as $i) 
+
+      foreach($images as $i)
       {
         $path = explode('.', ${'i'});
         $imname = explode('/', $path[0]);
-        
+
         //echo "<span style=\"text-align:left\";><h3><a href=\"${i}\">$imname[$imndx]</a></h3></span>" ;
-        echo "<a href=\"${i}\"><img src=\"${i}\" width=\"100%\" align=\"center\"</img></a>" ;
+        echo "<a href=\"${i}\"><img src=\"${i}\" width=\"100%\" align=\"center\"title=\"Dark gray bars are 1 standard deviation from the mean. Light gray bars are 2 standard deviations from the mean\"</img></a>" ;
         echo "<p>&nbsp;</p><hr> &nbsp;";
 
       }
 
-          //      // TODO : Display the mrq figures if there are no summary images
-          //      if ( (is_dir($figures_dir . "/" . $mrqdir)) && (!file_exists($figures_dir . "/" . $session . "/" . $fibers_gif) ) ) {   
-          //        
-          //        echo "<h2>mrQ Maps</h2>";
-          //        $images = glob($figures_dir . "/" . $mrqdir . "/" . "*.png");
-          //        
-          //          // Display each image with the name of the image
-          //          foreach($images as $i) {
-          //            $path = explode('.', ${'i'});
-          //            $imname = explode('/', $path[0]);
-          //            
-          //            // Display the images 
-          //            echo "<span style=\"text-align:left\";><h3><a href=\"${i}\">$imname[2]</a></h3></span>" ;
-          //            echo "<a href=\"${i}\"><img src=\"${i}\" width=\"100%\" align=\"center\"</img></a>" ;
-          //           echo "<p>&nbsp;</p><hr> &nbsp;";
-          //          }
-          //      }
-
-          //      // If the processing of AFQ is not complete then show the dti figures 
-          //      // (I don't think we want this)
-          //      if (is_dir($figures_dir . "/" . $session . "/" . strtolower($dtidir)) && (!file_exists($figures_dir . "/" . $session . "/" . $fibers_gif) )) {
-          //        
-          //        echo "<h2>DTI Images </h2>";
-          //        $images = glob($figures_dir . "/" . $session . "/" . strtolower($dtidir) . "/" . "*.png");
-          //        
-          //          // Display each image with the name of the image
-          //          foreach($images as $i) {
-          //            $path = explode('.', ${'i'});
-          //            $imname = explode('/', $path[0]);
-          //            
-          //            // Display the images 
-          //            echo "<span style=\"text-align:left\";><h3><a href=\"${i}\">$imname[3]</a></h3></span>" ;
-          //            echo "<a href=\"${i}\"><img src=\"${i}\" width=\"100%\" align=\"center\"</img></a>" ;
-          //            echo "<p>&nbsp;</p><hr> &nbsp;";
-          //          }
-          //    }
-
     }
 ?>
+
